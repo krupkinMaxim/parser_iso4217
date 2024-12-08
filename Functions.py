ce91ef0c8019d2187002983c7e93c5044ef0477b
Andrey1123413 <dimasous620@gmail.com>
2024-12-08 22:47:06 +0300

    Добавил парсинг и поиск таблицы в функцию

--- Functions.py
+++ Functions.py
@@ -8,3 +8,8 @@ def get_currency_data(url):
     response = requests.get(url)
     response.raise_for_status()  # Проверка на успешный запрос
 
+    # Парсинг страницы
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Находим таблицу с кодами валют
+    table = soup.find('table', {'class': 'wikitable'})
