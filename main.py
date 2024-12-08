161f6a2cfccf3f87becacb8ca6d20adc5930a0f0
Andrey1123413 <dimasous620@gmail.com>
2024-12-08 22:57:18 +0300

    Закончил main.py и functions.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from Functions import get_currency_data
-
-# Основной код
-if __name__ == "__main__":
-    main()
-    
+from Functions import get_currency_data 
  
+# Основной код 
+if __name__ == "__main__": 
+    url = 'https://en.wikipedia.org/wiki/ISO_4217'  # URL страницы 
+    df = get_currency_data(url)  # Получаем данные
