01bdc0930dde3753394c099ef577fca21796136b
Andrey1123413 <dimasous620@gmail.com>
2024-12-08 23:15:23 +0300

    Сделал слияние веток

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ from Functions import get_currency_data
 if __name__ == "__main__": 
     url = 'https://en.wikipedia.org/wiki/ISO_4217'  # URL страницы 
     df = get_currency_data(url)  # Получаем данные
+    
