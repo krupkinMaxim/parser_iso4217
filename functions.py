15c97487d8ecd51c3465b0ef03e3518f0dd237fc
Krupkin Maksim <krupkinmacsim@yandex.ru>
2024-12-10 19:44:47 +0300

    Создал функцию создания таблицы и сохранения в нее данных

--- functions.py
+++ functions.py
@@ -1,3 +1,8 @@
 import requests 
 from bs4 import BeautifulSoup 
 import pandas as pd
+
+def save_to_excel(df, file_name): 
+    """Функция для сохранения DataFrame в Excel файл.""" 
+    df.to_excel(file_name, index=False, engine='openpyxl') 
+    print(f"Таблица успешно сохранена в файл: {file_name}")
